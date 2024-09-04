64ea1dc24afdb5bc4e93ca32673c8655dcc907a0
Meenal0 <jainmeenal2008@gmail.com>
2024-09-04 23:01:52 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+let resetBtn = document.querySelector("#reset-btn");
+resetBtn.addEventListener("click", resetGame);
+
+const resetGame = "Hey there!";
+    msgContainer.classList.add("hide");
+    msgContainer.classList.remove("hide");
+    
+game.eventListener("Hello!");
